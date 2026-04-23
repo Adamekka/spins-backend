6430c51f2c285f1ce62a63641c020294abf0bdb7
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2026-04-23 16:43:04 +0200

    init

---
+++ src/main/java/com/adamekka/spins_backend/SpinsBackendApplication.java
@@ -0,0 +1,11 @@
+package com.adamekka.spins_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpinsBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpinsBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/adamekka/spins_backend/SpinsBackendApplicationTests.java
@@ -0,0 +1,10 @@
+package com.adamekka.spins_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpinsBackendApplicationTests {
+    @Test
+    void contextLoads() {}
+}
